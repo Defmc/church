c9e9a3bfa7aea12a01e35a0da446fcca7816f8e1
Defmc <defmc0101@gmail.com>
2024-02-13 10:04:52 -0400

    fix(bin): remove useless code

--- src/main.rs
+++ src/main.rs
@@ -20,48 +20,6 @@ fn repl() -> Result<(), Box<dyn std::error::Error>> {
     repl.start()
 }
 
-// fn info(mut expr: String, scope: &mut Scope, last: &mut Body) {
-//     let new_scope = Scope::from_str(&expr).unwrap();
-//     if new_scope.defs.is_empty() {
-//         let origin = expr.clone();
-//         let delta = scope.delta_redex(&mut expr);
-//         println!("expr: {origin}");
-//         println!("\tδ-eq:    {}", !delta);
-//         println!("\tδ-redex: {expr}");
-//         let lex = church::parser::lexer(&expr);
-//         match church::parser::parse(lex) {
-//             Ok(expr) => {
-//                 println!(
-//                     "\tδ-match: {}",
-//                     scope.get_from_alpha_key(&expr).unwrap_or("n/a")
-//                 );
-//                 println!("\tα-eq:    {}", last.alpha_eq(&expr));
-//                 println!("\tα-redex: {}", expr.clone().alpha_reduced());
-//                 println!(
-//                     "\t   -> β:  {}",
-//                     expr.clone().alpha_reduced().beta_reduced()
-//                 );
-//                 println!("\tβ-redex: {}", expr.clone().beta_reduced());
-//                 println!(
-//                     "\t   -> α:  {}",
-//                     expr.clone().beta_reduced().alpha_reduced()
-//                 );
-//                 println!(
-//                     "\tβ-match: {}",
-//                     scope
-//                         .get_from_alpha_key(&expr.clone().beta_reduced())
-//                         .unwrap_or("n/a")
-//                 );
-//                 // TODO: Match system
-//                 *last = expr;
-//             }
-//             Err(e) => println!("\terror:   {e:?}"),
-//         }
-//     } else {
-//         scope.extend(new_scope);
-//     }
-// }
-
 fn bootstrap() -> Result<(), Box<dyn std::error::Error>> {
     let src = include_str!("parser/parser.wop");
     let mut builder = wop::builder::Builder::from_str(src).unwrap();
