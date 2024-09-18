64aeb88cffbbeb95fbcece146c1fef207278e242
Defmc <defmc0101@gmail.com>
2024-09-18 12:11:28 -0300

    feat: support arguments as commands

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -1,5 +1,24 @@
+use std::env;
+
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     let mut repl = repl::Repl::default();
-    repl.run()
+
+    let mut args = env::args().skip(1).peekable();
+    if let Some(next) = args.peek() {
+        if let Ok(meta) = std::fs::metadata(next) {
+            if meta.is_file() {
+                todo!("run binary modules")
+            } else {
+                panic!("the binary module should be a standalone file");
+            }
+        } else {
+            for arg in args {
+                repl.handle(&arg);
+            }
+            Ok(())
+        }
+    } else {
+        repl.run()
+    }
 }
