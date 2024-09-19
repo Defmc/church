809a8f65518b643ebadb1f239b4b35c8be4f1ca1
Defmc <defmc0101@gmail.com>
2024-09-19 11:41:49 +0000

    fix: re-start tests

--- front/src/parser.rs
+++ front/src/parser.rs
@@ -78,40 +78,35 @@ pub type LexerTy = (std::result::Result<Token, ()>, Range<usize>);
 pub type Result<T> = std::result::Result<T, ParseError<usize, Token, ()>>;
 pub type Error = lalrpop_util::ParseError<usize, Token, ()>;
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::parser;
-//     use church::{assert_alpha_eq, assert_alpha_ne, Body, Term};
-//
-//     fn assert_ast_eq(lhs: &str, rhs: &str) {
-//         assert_alpha_eq!(
-//             Term::try_from(parser::try_from_str(lhs).unwrap()).unwrap(),
-//             Term::try_from(parser::try_from_str(rhs).unwrap()).unwrap()
-//         )
-//     }
-//
-//     fn assert_ast_ne(lhs: &str, rhs: &str) {
-//         assert_alpha_ne!(
-//             Term::try_from(parser::try_from_str(lhs).unwrap()).unwrap(),
-//             Term::try_from(parser::try_from_str(rhs).unwrap()).unwrap()
-//         )
-//     }
-//
-//     #[test]
-//     fn id() {
-//         let parsed: Term = parser::try_from_str("λx.x").unwrap().try_into().unwrap();
-//         let built: Term = Body::Abs(0, Body::Var(0).into()).into();
-//         assert_alpha_eq!(built, parsed);
-//     }
-//
-//     #[test]
-//     fn greedy_expr() {
-//         assert_ast_eq("λx.x λy.y x", "λx . (x λy.y x)");
-//     }
-//
-//     #[test]
-//     fn right_assoc_app() {
-//         assert_ast_eq("a b (c d) e f", "(((a b) (c d)) e) f");
-//         assert_ast_ne("a b (c d) e f", "a (((b (c d)) e) f)");
-//     }
-// }
+#[cfg(test)]
+mod tests {
+    use crate::{cu::CodeUnit, grammar::ExprParser};
+    use church::{assert_alpha_eq, assert_alpha_ne, Term};
+
+    fn dump_expr(src: &str) -> Term {
+        let tks = CodeUnit::into_tokens(src).unwrap();
+        let expr = ExprParser::new().parse(tks).unwrap();
+        let mut cu = CodeUnit::default();
+        cu.scope.dump(&expr).unwrap()
+    }
+
+    fn assert_alpha_eq(lhs: &str, rhs: &str) {
+        assert_alpha_eq!(dump_expr(lhs), dump_expr(rhs))
+    }
+
+    fn assert_alpha_ne(lhs: &str, rhs: &str) {
+        assert_alpha_ne!(dump_expr(lhs), dump_expr(rhs))
+    }
+
+    #[test]
+    fn greedy_expr() {
+        assert_alpha_eq("λx => x λy => y x", "λx => (x λy => (y x))");
+        assert_alpha_eq("λx => x λy => y x", "fn x => x fn y => y x");
+    }
+
+    #[test]
+    fn right_assoc_app() {
+        assert_alpha_eq("a b (c d) e f", "(((a b) (c d)) e) f");
+        assert_alpha_ne("a b (c d) e f", "a (((b (c d)) e) f)");
+    }
+}
